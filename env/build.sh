816d92821798213706464dbbf6909a0eeab2a473
Vyacheslav Kuzmin <vyacheslav.v.kuzmin@gmail.com>
2023-11-19 11:46:46 +0100

    Get rid of lines and add a test

--- env/build.sh
+++ env/build.sh
@@ -1,10 +1,1 @@
 runuser -u red -c 'poetry export --output requirements.txt --without-hashes'
-docker build -f env/Dockerfile -t armann/telegate:latest .
-docker stop telegate
-docker rm telegate
-mkdir -p /opt/containers/telegate/data
-cp -f env/docker-compose.yml  /opt/containers/telegate/
-cp -f /home/red/telegate/telegate.env  /opt/containers/telegate/data/
-chown -R cont:cont /opt/containers/telegate
-chmod -R g+rw /opt/containers/telegate
-docker compose -f /opt/containers/telegate/docker-compose.yml up -d --force-recreate
--- tests/test_msg1.sh
+++ tests/test_msg1.sh
@@ -1,2 +1,3 @@
 curl -d '{"receiver": "@cosimu", "body": "Hey!"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/api/message
+curl -d '{"receiver": "#Monitoring", "body": "It is an impossible event"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/api/message
 
