686d59ab0b4d7aad87fedc15fcbae752183a600e
Vyacheslav Kuzmin <vyacheslav.v.kuzmin@gmail.com>
2023-11-19 12:10:23 +0100

    Redesign API

--- api.py
+++ api.py
@@ -3,19 +3,12 @@ import logging
 
 from fastapi import APIRouter
 from fastapi import FastAPI
-from pydantic import BaseModel
+from starlette.requests import Request
 from starlette.types import Receive
 from starlette.types import Scope
 from starlette.types import Send
 
 from gram import Gram
-
-_logger = logging.getLogger(__name__)
-
-
-class Message(BaseModel):
-    receiver: str
-    body: str
 
 
 class Api:
@@ -23,7 +16,7 @@ class Api:
         self._gram = gram
         self._fastapi = FastAPI(on_startup=[self._start_gram])
         self._router = APIRouter()
-        self._router.add_api_route("/message", self._post_message, methods=["POST"])
+        self._router.add_api_route("/message/{receiver}", self._post_message, methods=["POST"])
         self._fastapi.include_router(self._router, prefix="/api")
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send):
@@ -33,5 +26,18 @@ class Api:
         asyncio.create_task(self._gram.connect())
         _logger.debug('Starting Gram')
 
-    async def _post_message(self, msg: Message):
-        await self._gram.send(msg.receiver, msg.body)
+    async def _post_message(self, receiver: str, request: Request):
+        """
+        Format of receiver:
+            started with @ - username
+            otherwise - group or channel name
+        """
+        if not receiver.startswith('@'):
+            receiver = '#' + receiver
+        body = await request.body()
+        body_decoded = body.decode('utf8')
+        _logger.debug(f"Got a message for {receiver}. Body:\n{body_decoded!r}")
+        await self._gram.send(receiver, body_decoded)
+
+
+_logger = logging.getLogger(__name__)
